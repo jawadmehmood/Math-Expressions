2d8556004e43410ba4ca34d5ac8b74eed9443503
jawadmehmood <jawadam5678@gmail.com>
2024-07-09 02:45:21 +0500

    Assignment 4 completed

---
+++ app.js
@@ -0,0 +1,75 @@
+// Assignment 1
+
+var num1 = 10;
+var num2 = 35;
+
+var sum = num1 + num2;
+console.log('Sum of ' + num1 + ' and ' + num2 + ' is ' + sum);
+document.write('Sum of ' + num1 + ' and ' + num2 + ' is ' + sum + '<br />');
+
+// Assignment 2
+
+var subtract = num1 - num2;
+console.log('Subtraction of ' + num1 + ' and ' + num2 + ' is ' + subtract);
+document.write('Subtraction of ' + num1 + ' and ' + num2 + ' is ' + subtract + '<br />');
+
+var product = num1 * num2;
+console.log('Product of ' + num1 + ' and ' + num2 + ' is ' + product);
+document.write('Product of ' + num1 + ' and ' + num2 + ' is ' + product + '<br />');
+
+var divide = num1 / num2;
+console.log('Division of ' + num1 + ' and ' + num2 + ' is ' + divide);
+document.write('Division of ' + num1 + ' and ' + num2 + ' is ' + divide + '<br />');
+
+var modulus = num1 % num2;
+console.log('Modulus of ' + num1 + ' and ' + num2 + ' is ' + modulus);
+document.write('Modulus of ' + num1 + ' and ' + num2 + ' is ' + modulus + '<br />');
+
+// Assignment 3
+
+var  num
+
+console.log('Value after variable declaration is ' + num);
+document.write('Value after variable declaration is ' + num + '<br />');
+
+num = 5;
+
+console.log('Initial value: ' + num);
+document.write('Initial value: ' + num + '<br />');
+
+var num = ++num;
+
+console.log('Value after increment is: ' + num);
+document.write('Value after increment is: ' + num + '<br />');
+
+var num = num +  7;
+
+console.log('Value after addition is: ' + num);
+document.write('Value after addition is: ' + num + '<br />');
+
+var num = --num;
+
+console.log('Value after decrement is: ' + num);
+document.write('Value after decrement is: ' + num + '<br />');
+
+var num = num % 3;
+
+console.log('The remainder is: ' + num);
+document.write('The remainder is: ' + num + '<br />');
+
+// Assignment 4
+
+var ticketPrice = 600;
+var noOfTickets = 5;
+var costOf5Tickets = ticketPrice * noOfTickets;
+
+console.log('Total cost to buy ' + noOfTickets + ' tickets to a movie is ' + costOf5Tickets + 'PKR');
+document.write('Total cost to buy ' + noOfTickets + ' tickets to a movie is ' + costOf5Tickets + 'PKR' + '<br />');
+
+// Assignment 5
+
+
+// Assignment 1
+// Assignment 1
+// Assignment 1
+// Assignment 1
